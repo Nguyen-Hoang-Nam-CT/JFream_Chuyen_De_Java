28f7a6dab49dd198791d883c0dc3ba2ebbfb0e69
your name <colammoicoan10@gmail.com>
2021-09-28 21:01:27 +0700

    Thêm buổi 2: thiết kế giao diện đăng nhập bằng code

---
+++ TH_Buoi2/Bai_4_Chuong_4_Giao_Dien_Dang_Nhap/src/bai_4_chuong_4_giao_dien_dang_nhap/Run.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai_4_chuong_4_giao_dien_dang_nhap;
+
+
+public class Run 
+{
+    public static void main(String[] args) 
+    {
+        Bai_4_Chuong_4_Giao_Dien_Dang_Nhap frame = new Bai_4_Chuong_4_Giao_Dien_Dang_Nhap();
+        frame.createGUI();
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+    }
+}
